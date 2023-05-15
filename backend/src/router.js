a8bb4b7d9b2a78e5f1090d63c0d97c3b213ba336
Marcus Vinicius <83254316+SirDarky@users.noreply.github.com>
2023-05-15 18:04:40 -0400

    lista de afazeres

--- backend/src/router.js
+++ backend/src/router.js
@@ -2,5 +2,16 @@ const router = require('express').Router();
 const userController = require("./controller/userController")
 
 router.use("/user", userController);
+//Falta:
+/*
+Adicionar Cursos
+Adicionar Experiencia
+Realizar uma nova conversa
+Realizar o matches
+Cadastro de empresa
+Cadastro de vagas
+Envio de mensagens(talvez por meio do websocket)
+
+*/
 
 module.exports = router
